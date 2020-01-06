418f4a96b40ffbfda424b4f39312f5bd939ace94
wuch1995 <wuchyuz@gmail.com>
2020-01-06 23:57:20 +0800

    update: panel/notices

--- src/views/panel/message/message.tsx
+++ src/views/panel/message/message.tsx
@@ -2,6 +2,34 @@ import React, { useState, useEffect } from "react"
 import './message.less'
 import classnames from 'classnames'
 import api from 'API/index'
+import Spin from 'COMPONENTS/spin/spin'
+import dayjs from 'dayjs'
+
+function getMessageLast (last: string) {
+  const lastMsg = JSON.parse(last)
+  return lastMsg.msg
+}
+
+function getNotice (noticeData) {
+  const notice = JSON.parse(noticeData.notice)
+  let msg
+  let content
+
+  if (notice.playlist) {
+    msg = '收藏了你的歌单'
+    content = `「${notice.playlist.name}」`
+  }
+  if (notice.comment) {
+    msg = '赞了你的评论'
+    content = notice.comment.content
+  }
+  return {
+    msg,
+    content,
+    user: notice.user,
+    time: dayjs(noticeData.time).format('YYYY年MM月DD日')
+  }
+}
 
 const Message: React.SFC = () => {
   const CURRENT_PLAYLIST_PANEL_TAB = {
@@ -10,9 +38,12 @@ const Message: React.SFC = () => {
     forward: '@我',
     notice: '通知'
   }
-  const [tab, setTab] = useState('comment')
+  const [tab, setTab] = useState('message')
   const [loading, setLoading] = useState(true)
   const [message, setMessage] = useState([])
+  const [comment, setComment] = useState([])
+  const [notice, setNotice] = useState([])
+
   const api_map = {
     message: api.getPanelMessage,
     comment: api.getPanelComments,
@@ -24,25 +55,114 @@ const Message: React.SFC = () => {
     getData()
   }, [tab])
 
-
   async function getData () {
     try {
       setLoading(true)
       const res = await api_map[tab]({ limit: 30, uid: 98931610 })
-      console.log(res)
+      switch (tab) {
+        case 'message':
+          setMessage(res.data.msgs)
+          break
+        case 'comment':
+          setComment(res.data.comments)
+          break
+        case 'forward':
+          break
+        case 'notice':
+          setNotice(res.data.notices)
+      }
     } finally {
       setLoading(false)
     }
   }
 
-  function genMessageNode () {
-
+  function genNode () {
+    switch (tab) {
+      case 'message':
+        return genMessageNode()
+      case 'comment':
+        return genCommentNode()
+      case 'forward':
+        break
+      case 'notice':
+        return genNoticeNode()
+    }
   }
+
+  function genCommentNode () {
+    return (
+      <ul className="panel-comment-list">
+        {
+          comment.map((item, index) => (
+            <li key={index} className="panel-comment-item">
+              <img className="panel-comment-avatar" src={item.user.avatarUrl} alt=""/>
+              <div className="panel-message-info">
+                <div className="panel-message-name">
+                  <span>{item.user.nickname}</span>
+                  <span>1月3日</span>
+                </div>
+                <div className="panel-comment-replay">回复我: {item.content}</div>
+                <div className="panel-comment-msg">{item.beRepliedContent}</div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
+  function genMessageNode () {
+    return (
+      <ul className="panel-message-list">
+        {
+          message.map((item, index) => (
+            <li key={index} className="panel-message-item">
+              <img className="panel-message-avatar" src={item.fromUser.avatarUrl} alt=""/>
+              <div className="panel-message-info">
+                <div className="panel-message-name">
+                  <span>{item.fromUser.nickname}</span>
+                  <span>1月3日</span>
+                </div>
+                <div className="panel-message-msg">{getMessageLast(item.lastMsg)}</div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
+  function genNoticeNode () {
+    return (
+      <ul className="panel-message-list">
+        {
+          notice.map((item, index) => {
+            const notice = getNotice(item)
+            return (
+              <li key={index} className="panel-message-item">
+                <img className="panel-message-avatar" src={notice.user.avatarUrl} alt=""/>
+                <div className="panel-message-info">
+                  <div className="panel-message-name">
+                    <span>{notice.user.nickname}{notice.msg}</span>
+                    <span>{notice.time}</span>
+                  </div>
+                  <div className="panel-message-msg">{notice.content}</div>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
 
   function selectTab (tab: string) {
+    setTab(tab)
   }
+
   return (
-    <div className="message-container">
+    <div className="message-panel-container">
       <div className="message-panel-tab ">
         {
           Object.keys(CURRENT_PLAYLIST_PANEL_TAB).map(key => (
@@ -51,6 +171,13 @@ const Message: React.SFC = () => {
             </div>
           ))
         }
+      </div>
+      <div className="message-panel-content">
+        <Spin loading={loading} delay={300}>
+          {
+            genNode()
+          }
+        </Spin>
       </div>
     </div>
   )

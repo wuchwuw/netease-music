0ce2a7b65887c2b97990c84aa9fa47623c608817
吴翀 <wuchyuz@gmail.com>
2020-01-19 15:57:24 +0800

    update: artist

--- src/views/home/artist/artist.tsx
+++ src/views/home/artist/artist.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import api from 'API/index'
+import './artist.less'
 
 const Artist: React.SFC = () => {
   const [artists, setArtists] = useState([])
@@ -17,6 +18,7 @@ const Artist: React.SFC = () => {
         offset: 0
       }
       const res = await api.getArtist(params)
+      setArtists(res.data.artists)
     } catch (e) {
       console.log(e)
     }
@@ -25,16 +27,42 @@ const Artist: React.SFC = () => {
   return (
     <div className="artist">
       <div className="artist-filter">
-        <div>
-          <span>语种</span>
-          <div></div>
+        <div className="artist-filter-item">
+          <span>语种:</span>
+          <div>
+            <span className="active">华语</span>
+            <span>欧美</span>
+            <span>日本</span>
+            <span>韩国</span>
+          </div>
         </div>
-        <div>
-          <span>分类</span>
+        <div className="artist-filter-item">
+          <span>分类:</span>
+          <div>
+            <span>男歌手</span>
+            <span>女歌手</span>
+            <span>乐队组合</span>
+          </div>
         </div>
-        <div>
-          <span>筛选</span>
+        <div className="artist-filter-item">
+          <span>筛选:</span>
+          <div>
+            <span>A</span>
+            <span>B</span>
+            <span>C</span>
+            <span>D</span>
+          </div>
         </div>
+      </div>
+      <div className="artist-content">
+        {
+          artists.map(artist => (
+            <div key={artist.id} className="artist-item">
+              <img src={artist.picUrl + '?param=250y250'} alt=""/>
+              <div>{artist.name}</div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )

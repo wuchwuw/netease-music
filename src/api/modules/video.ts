60efd01ff5e3c34a520940a115be6f7054351e00
wuch1995 <wuchyuz@gmail.com>
2020-01-24 23:54:57 +0800

    update: video

--- src/api/modules/video.ts
+++ src/api/modules/video.ts
@@ -6,5 +6,14 @@ export default {
   },
   getViodeList (params: any) {
     return axios.get('video/group', { params, withCredentials: true })
+  },
+  getAllMv (params: any) {
+    return axios.get('mv/all', { params })
+  },
+  getExclusiveMv (params: any) {
+    return axios.get('mv/exclusive/rcmd', { params })
+  },
+  getNewMv (params: any) {
+    return axios.get('mv/first', { params })
   }
 }

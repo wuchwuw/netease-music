ed4ff34d0f453fbbfe0aa0c562ce3c44caa644e0
wuch1995 <wuchyuz@gmail.com>
2020-04-25 23:57:19 +0800

    update: refresh cookie

--- src/util/login.ts
+++ src/util/login.ts
@@ -1,8 +1,16 @@
+import api from 'API/index'
+
 export function checkLoginStatus () {
   const cookie = document.cookie
   return cookie.indexOf('MUSIC_U=') > -1
 }
 
-export function refresh () {
-  // todo
+export async function refresh () {
+  if (checkLoginStatus()) {
+    try {
+      await api.refreshLogin()
+    } catch (e) {}
+  } else {
+    // todo clear localstorage
+  }
 }
